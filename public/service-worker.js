48a364a7f16504534f3ea9b1b033edb8683a34f7
Usama <usamakha2003@gmail.com>
2025-08-18 10:38:09 +0500

    added manifest and serviceWorker

---
+++ public/service-worker.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-restricted-globals */
+
+const CACHE_NAME = "time-app-cache-v1";
+const urlsToCache = ["/", "/index.html", "/manifest.json"];
+
+// Install SW
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate SW
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Fetch (offline support)
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
